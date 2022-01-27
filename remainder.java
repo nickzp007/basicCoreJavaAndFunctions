8ee374824eaa01a2ade1463ca77e55b04d085711
unknown <nikhil98patil@gmail.com>
2022-01-27 22:52:51 +0530

    Added Quotient and remainder computation program

---
+++ remainder.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.corejava;
+
+import java.util.Scanner;
+
+public class remainder {
+
+	public static void main(String[] args) {
+		int numerator, denominator;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter numerator:");
+		numerator=sc.nextInt();
+		System.out.println("Enter denominator:");
+		denominator=sc.nextInt();
+		System.out.println("Quotient: "+numerator/denominator);
+		System.out.println("Remainder: "+numerator%denominator);
+		sc.close();
+	}
+
+}
+
+
