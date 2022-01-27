eefbb7ad8dad3efbf54a6257457e7f370f425fd4
unknown <nikhil98patil@gmail.com>
2022-01-27 22:46:23 +0530

    Added program for computing powers of 2

---
+++ powerOfTwo.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.corejava;
+
+import java.util.*;
+public class powerOfTwo {
+
+	public static void main(String[] args) {
+		int N, powerOfTwo, i;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Power Value Between 0 to 31: ");
+		N = sc.nextInt();
+		sc.close();
+
+		for (i = 1; i <= N; i++) {
+			powerOfTwo = (int) Math.pow(2, i);
+			System.out.println(powerOfTwo + " ");
+		}
+	}
+}
