8c0bc22bffd6f30b40e9b89e10801bb3bc6a7bbd
unknown <nikhil98patil@gmail.com>
2022-01-28 19:41:25 +0530

    Added sumzero program

---
+++ New folder/sumZero.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.functional;
+
+
+public class sumZero {
+	public static void main(String[] args) {
+
+		int Arr[] = {1, 2, 4, -3, -1, 5, 6};
+
+
+		for (int i=0; i<Arr.length; i++) {
+
+			for (int j=i+1; j<Arr.length; j++) {
+
+				for (int k=j+1; k<Arr.length; k++) {
+
+					if(Arr[i] + Arr[j] +Arr[k] == 0) {
+
+						System.out.println("Numbers Found = "+Arr[i]+ "," +Arr[j]+ "," +Arr[k]);
+					}
+				}
+			}
+		}
+	}
+}
